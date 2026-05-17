fbb6fe8c5f9b9dda340d05e2f7bd0866d34b4799
Tim <TimLiang726@gmail.com>
2026-05-17 14:26:00 -0400

    Init project setup

---
+++ backend/src/test/java/io/github/timliiang/MovieReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.io.github.timliiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
